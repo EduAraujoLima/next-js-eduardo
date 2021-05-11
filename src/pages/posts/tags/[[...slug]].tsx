70ed843d8b0c4a630d8b92a4d3bc5fb2c6091813
eduardo.lima <edu.wp@Outlook.com>
2021-05-11 17:32:07 -0300

    Refatoracao layout

--- src/pages/posts/tags/[[...slug]].tsx
+++ src/pages/posts/tags/[[...slug]].tsx
@@ -1,13 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from "next";
 import Layout from "../../../components/Layout";
 import BasicMeta from "../../../components/meta/BasicMeta";
-import OpenGraphMeta from "../../../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../../../components/meta/TwitterCardMeta";
 import TagPostList from "../../../components/TagPostList";
 import config from "../../../lib/config";
 import { countPosts, listPostContent, PostContent } from "../../../lib/posts";
 import { getTag, listTags, TagContent } from "../../../lib/tags";
-import Head from "next/head";
 
 type Props = {
   posts: PostContent[];
@@ -24,8 +21,6 @@ export default function Index({ posts, tag, pagination, page }: Props) {
   return (
     <Layout>
       <BasicMeta url={url} title={title} />
-      <OpenGraphMeta url={url} title={title} />
-      <TwitterCardMeta url={url} title={title} />
       <TagPostList posts={posts} tag={tag} pagination={pagination} />
     </Layout>
   );
